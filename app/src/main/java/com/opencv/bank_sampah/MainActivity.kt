163d7f4da36e5756b7fafdf8cbf66e3ade666079
Kaleb <molewekaleb33@gmail.com>
2023-05-12 13:07:39 +0700

    memulai

---
+++ app/src/main/java/com/opencv/bank_sampah/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.opencv.bank_sampah
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
